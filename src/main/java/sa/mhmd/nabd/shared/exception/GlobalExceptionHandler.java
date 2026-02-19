93ee831d4f56e994f63739c74a37a48ec1eb2c51
MhmdFayedh <mhmdfayedh@gmail.com>
2026-02-19 14:03:16 +0300

    Integrate `.env` support and externalize configuration variables in `application.yaml` and `docker-compose.yml`.

--- src/main/java/sa/mhmd/nabd/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/sa/mhmd/nabd/shared/exception/GlobalExceptionHandler.java
@@ -5,13 +5,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import sa.mhmd.nabd.shared.NabdErrorResponse;
+import sa.mhmd.nabd.shared.NabdResponse;
+import sa.mhmd.nabd.shared.enums.Errors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NabdApplicationException.class)
     public ResponseEntity<NabdErrorResponse> handleNabdApplicationException(
             NabdApplicationException ex) {
-        NabdErrorResponse errorResponse = NabdErrorResponse.failure(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        Errors error = Errors.fromCode(ex.getErrorCode());
+
+        return ResponseEntity.status(error.getStatus()).body(NabdResponse.failure(ex.getErrorCode()));
+    }
+
+    @ExceptionHandler(NabdBusinessException.class)
+    public ResponseEntity<?> handleNabdApplicationException(
+            NabdBusinessException ex) {
+        Errors error = Errors.fromCode(ex.getErrorCode());
+
+        return ResponseEntity.status(error.getStatus()).body(NabdResponse.failure(ex.getErrorCode()));
     }
 }
